3cc8b4d2cc7d8863984e08b2454ae5d4b32b6a28
Anton <kemenist@hotmail.com>
2018-04-08 03:21:53 +0200

    Saturday night

--- RandomGraph/Form1.Designer.cs
+++ RandomGraph/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -39,60 +39,79 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
+            this.chart1.BackColor = System.Drawing.SystemColors.Control;
+            this.chart1.BorderlineColor = System.Drawing.SystemColors.Control;
+            this.chart1.BorderSkin.PageColor = System.Drawing.SystemColors.Control;
             chartArea1.Name = "ChartArea1";
+            chartArea1.Visible = false;
             this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(90, 39);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series1.BorderWidth = 2;
             series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series1.Name = "Random";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(200)))));
+            series2.Name = "Average";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(602, 300);
-            this.chart1.TabIndex = 0;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(984, 538);
+            this.chart1.TabIndex = 8;
+            this.chart1.TabStop = false;
             this.chart1.Text = "chart1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(617, 387);
+            this.button1.Location = new System.Drawing.Point(701, 553);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
+            this.button1.TabIndex = 0;
             this.button1.Text = "Generate";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(90, 390);
+            this.textBox1.Location = new System.Drawing.Point(174, 556);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            this.textBox1.GotFocus += new System.EventHandler(this.textBox1_GotFocus);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(269, 390);
+            this.textBox2.Location = new System.Drawing.Point(353, 556);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "100";
+            this.textBox2.GotFocus += new System.EventHandler(this.textBox2_GotFocus);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(449, 390);
+            this.textBox3.Location = new System.Drawing.Point(533, 556);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 4;
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "200";
+            this.textBox3.GotFocus += new System.EventHandler(this.textBox3_GotFocus);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 374);
+            this.label1.Location = new System.Drawing.Point(171, 540);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(24, 13);
             this.label1.TabIndex = 5;
@@ -101,7 +120,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(266, 374);
+            this.label2.Location = new System.Drawing.Point(350, 540);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(27, 13);
             this.label2.TabIndex = 6;
@@ -110,17 +129,39 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(446, 374);
+            this.label3.Location = new System.Drawing.Point(530, 540);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Iterations";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(809, 556);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Loop";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(425, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Click generate to show diagram";
+            // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 601);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -129,12 +170,19 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "RandomGraph";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
+        }
+
+        private void TextBox1_GotFocus(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         #endregion
@@ -147,6 +195,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- RandomGraph/Form1.cs
+++ RandomGraph/Form1.cs
@@ -15,18 +15,43 @@ namespace RandomGraph
     {
         private void button1_Click(object sender, EventArgs e)
         {
-            var min = int.Parse(textBox1.Text);
-            var max = int.Parse(textBox1.Text);
-            var iterations = int.Parse(textBox1.Text);
+            var min = 0;
+            var max = 0;
+            var iterations = 0;
 
-            var randomArray = GenerateRandomArray(min, max, iterations);
-            var randomMean = new DataPoint[randomArray.Length];
-
-            for (var i = 0; i < randomArray.Length; i++)
+            try
             {
-                randomMean[i]  randomArray.Take(i + 1).Average();
+                min = int.Parse(textBox1.Text);
+                max = int.Parse(textBox2.Text);
+                iterations = int.Parse(textBox3.Text);
+
+                var randomArray = GenerateRandomArray(min, max, iterations);
+                var randomMean = new DataPoint[randomArray.Length];
+
+                //chart1.ChartAreas["ChartArea1"].AxisX.IsLogarithmic = true;
+                
+                chart1.Series["Average"].Points.Clear();
+                chart1.Series["Random"].Points.Clear();
+
+                chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+                chart1.ChartAreas["ChartArea1"].AxisX.Maximum = iterations;
+                chart1.ChartAreas["ChartArea1"].AxisY.Minimum = min;
+                chart1.ChartAreas["ChartArea1"].AxisY.Maximum = max;
+                chart1.ChartAreas["ChartArea1"].AxisY.Interval = (double)(max - min) / 4;
+
+                for (var i = 0; i < randomArray.Length; i++)
+                {
+                    chart1.Series["Average"].Points.AddXY(i + 1, randomArray.Take(i + 1).Average());
+                    chart1.Series["Random"].Points.AddXY(i + 1, randomArray[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Invalid input");
             }
 
+            label4.Visible = false;
+            chart1.ChartAreas["ChartArea1"].Visible = true;
         }
 
         public Form1()
@@ -42,6 +67,29 @@ namespace RandomGraph
             for (var i = 0; i < iterations; i++) randomIntArray[i] = random.Next(min, max + 1);
 
             return randomIntArray;
+        }
+
+        private void textBox1_GotFocus(object sender, EventArgs e)
+        {
+            textBox1.SelectAll();
+        }
+
+        private void textBox2_GotFocus(object sender, EventArgs e)
+        {
+            textBox2.SelectAll();
+        }
+
+        private void textBox3_GotFocus(object sender, EventArgs e)
+        {
+            textBox3.SelectAll();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+                Close();
+
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
